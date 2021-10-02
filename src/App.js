c804b35085346858a7a7f7c23da39f1507e3e2eb
Magomed Shamaev <ms77grz@gmail.com>
2021-10-02 21:37:13 +0300

    Sticky Columns

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { ColumnHiding } from './components/ColumnHiding';
+import { StickyTable } from './components/StickyTable';
 
 function App() {
   return (
     <div className='App'>
-      <ColumnHiding />
+      <StickyTable />
     </div>
   );
 }
--- src/components/columns.js
+++ src/components/columns.js
@@ -8,18 +8,21 @@ export const COLUMNS = [
     accessor: 'id',
     // Filter: ColumnFilter,
     disableFilters: true,
+    sticky: 'left',
   },
   {
     Header: 'First Name',
     Footer: 'First Name',
     accessor: 'first_name',
     // Filter: ColumnFilter,
+    sticky: 'left',
   },
   {
     Header: 'Last Name',
     Footer: 'Last Name',
     accessor: 'last_name',
     // Filter: ColumnFilter,
+    sticky: 'left',
   },
   {
     Header: 'Date of Birth',
@@ -41,6 +44,16 @@ export const COLUMNS = [
     Footer: 'Phone',
     accessor: 'phone',
     // Filter: ColumnFilter,
+  },
+  {
+    Header: 'Email',
+    Footer: 'Email',
+    accessor: 'email',
+  },
+  {
+    Header: 'Age',
+    Footer: 'Age',
+    accessor: 'age',
   },
 ];
 
